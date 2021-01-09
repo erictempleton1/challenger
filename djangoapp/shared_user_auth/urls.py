4e593d4b5a4ec2f51a4766c4bd41cd8699f7d4c9
Eric Templeton <erictempleton1@users.noreply.github.com>
2021-01-09 18:44:14 +0000

    adding user registration and basic profile page

--- djangoapp/shared_user_auth/urls.py
+++ djangoapp/shared_user_auth/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
+from shared_user_auth.views import RegisterView, Profile
 
 
 app_name = 'shared_user_auth'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name="login"),
+    path('register/', RegisterView.as_view(), name="register"),
+    path('profile/', Profile.as_view(), name="profile"),
 ]
 
 # TODO - create register and profile views. Use challenger repo for examples!
--- djangoapp/shared_user_auth/views.py
+++ djangoapp/shared_user_auth/views.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.shortcuts import render
+from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class RegisterView(FormView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/create_user.html'
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return super().form_valid(form)
+
+
+class Profile(TemplateView):
+    template_name = "user/profile.html"
