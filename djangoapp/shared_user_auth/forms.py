6e9a3d8623560963ef6e27745a63fac2a84a9653
Eric Templeton <erictempleton1@users.noreply.github.com>
2021-01-26 01:15:18 +0000

    working on reg form

--- djangoapp/shared_user_auth/forms.py
+++ djangoapp/shared_user_auth/forms.py
@@ -6,7 +6,7 @@ class UserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
 
-    username = forms.EmailField(widget=forms.TextInput(
+    username = forms.CharField(widget=forms.TextInput(
         attrs={'class': 'input is-primary', 'placeholder': 'petersagan', 'id': 'username'}))
     password = forms.CharField(widget=forms.PasswordInput(
         attrs={'class': 'input is-primary', 'placeholder': '*********', 'id': 'password'}))
@@ -15,3 +15,34 @@ class UserLoginForm(AuthenticationForm):
 class CustomUserCreationForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input is-primary',
+                'placeholder': 'petersagan',
+                'id': 'username'
+            }))
+    password1 = forms.CharField(
+        label="Password",
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'autocomplete': 'new-password',
+                'class': 'input is-primary',
+                'id': 'password',
+                'placeholder': '******'
+            }),
+    )
+    password2 = forms.CharField(
+        label="Confirm Password",
+        strip=False,
+        widget=forms.PasswordInput(
+            attrs={
+                'autocomplete': 'new-password',
+                'class': 'input is-primary',
+                'id': 'confirm-password',
+                'placeholder': '******'
+            }),
+    )
